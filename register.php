1411cab5b1a371fb61203b6a87050c4ad6dd824f
HarshanaEshan <harshanaeshan.info@gmail.com>
2024-02-10 09:18:46 +0530

    'host-gitpages'

---
+++ register.php
@@ -0,0 +1,13 @@
+<?php  
+
+    $conn = mysqli_connect("localhost","root","");
+        if (!$conn)
+        {
+            die('Could not connect: ' . mysqli_error($conn));
+        }
+        mysqli_select_db($conn,"learnme_db");
+        $sql="INSERT INTO regtable values ('$_POST[uname]','$_POST[pword]') ";
+        mysqli_query($conn,$sql);
+    mysqli_close($conn);
+
+?>
